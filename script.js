f72c2fec562a3f410d76f56331fe7e1b29c93f0f
aiswaryadineshan <110421893+aiswaryadineshan@users.noreply.github.com>
2022-08-02 22:57:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function changecontent1()
+{
+    let x=document.getElementById("open-this1").style;
+    
+    if(x.display=="block"){
+        x.display="none";
+    }
+    else{
+        x.display="block";
+    }
+}
+
+function changecontent2()
+{
+    let x=document.getElementById("open-this2").style;
+    
+    if(x.display=="block"){
+        x.display="none";
+    }
+    else{
+        x.display="block";
+    }
+}
+
+function changecontent3()
+{
+    let x=document.getElementById("open-this3").style;
+    
+    if(x.display=="block"){
+        x.display="none";
+    }
+    else{
+        x.display="block";
+    }
+}
+
+function changecontent4()
+{
+    let x=document.getElementById("open-this4").style;
+    
+    if(x.display=="block"){
+        x.display="none";
+    }
+    else{
+        x.display="block";
+    }
+}
+
+function changecontent5()
+{
+    let x=document.getElementById("open-this5").style;
+    
+    if(x.display=="block"){
+        x.display="none";
+    }
+    else{
+        x.display="block";
+    }
+}
